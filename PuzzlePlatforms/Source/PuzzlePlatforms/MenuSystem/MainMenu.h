b9976974ce9c814b9f363b57b1e79e0f06c26237
ricky2511 <rickyto2511@gmail.com>
2019-01-05 05:04:49 +0800

    Lecture37

--- PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MainMenu.h
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MenuSystem/MainMenu.h
@@ -30,13 +30,31 @@ protected:
 private:
 
 	UPROPERTY(meta = (BindWidget))
-	class UButton* Host;
+	class UButton* HostButton;
 
 	UPROPERTY(meta = (BindWidget))
-	class UButton* Join;
+	class UButton* JoinButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* CancelJoinMenuButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UWidgetSwitcher* MenuSwitcher;
+
+	UPROPERTY(meta = (BindWidget))
+	class UWidget* JoinMenu;
+
+	UPROPERTY(meta = (BindWidget))
+	class UWidget* MainMenu;
 
 	UFUNCTION()
 	void HostServer();
+
+	UFUNCTION()
+	void OpenJoinMenu();
+
+	UFUNCTION()
+	void OpenMainMenu();
 
 	IMenuInterface* MenuInterface;
 };
